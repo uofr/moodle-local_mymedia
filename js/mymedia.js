9c298d2f173ad907936273e8a55afa83f2937ca7
trevor-c <trevor.cunningham@uregina.ca>
2014-01-27 12:03:11 -0600

    localizing yui support files; using https protocol

--- js/mymedia.js
+++ js/mymedia.js
@@ -112,7 +112,7 @@ M.local_mymedia.init_config = function (Y, panel_markup, dialog, conversion_scri
                                                ); 
 
     loading_panel.setHeader("Loading, please wait..."); 
-    loading_panel.setBody('<img src="http://l.yimg.com/a/i/us/per/gr/gp/rel_interstitial_loading.gif" />');
+    loading_panel.setBody('<img src="https://urcourses.uregina.ca/local/kaltura/pix/rel_interstitial_loading.gif" />');
     loading_panel.render();
 
     // Create preview panel
